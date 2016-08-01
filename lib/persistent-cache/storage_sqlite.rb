76bf50117387bdc5080e896706be973968f72691
Ernst Van Graan <ernst.van.graan@hetzner.co.za>
2016-08-01 11:46:50 +0200

    Code style

--- lib/persistent-cache/storage_sqlite.rb
+++ lib/persistent-cache/storage_sqlite.rb
@@ -22,38 +22,38 @@ module Persistent
     def save_key_value_pair(key, value, timestamp = nil)
       delete_entry(key)
       time_entry = timestamp.nil? ? Time.now.to_s : timestamp.to_s
-      EH::retry!(:args => [serialize(key), serialize(value), time_entry]) do
+      EH.retry!(:args => [serialize(key), serialize(value), time_entry]) do
         @storage_handler.execute("INSERT INTO #{DB_TABLE} (key, value, timestamp) VALUES(?, ?, ?)",serialize(key), serialize(value), time_entry)
       end
     end
 
     def lookup_key(key)
-      EH::retry!(:args => [serialize(key)]) do
+      EH.retry!(:args => [serialize(key)]) do
         result = @storage_handler.execute("SELECT value, timestamp FROM #{DB_TABLE} WHERE key=?", serialize(key))
         !result.nil? && !result.empty? ? [deserialize(result[0][0]), result[0][1]] : nil
       end
     end
 
     def delete_entry(key)
-      EH::retry!(:args => [serialize(key)]) do
+      EH.retry!(:args => [serialize(key)]) do
         @storage_handler.execute("DELETE FROM #{DB_TABLE} WHERE key=?", serialize(key))
       end
     end
 
     def size
-      EH::retry!(:args => []) do
+      EH.retry!(:args => []) do
         @storage_handler.execute("SELECT value FROM #{DB_TABLE}").size
       end
     end
 
     def keys
-      EH::retry!(:args => []) do
+      EH.retry!(:args => []) do
         @storage_handler.execute("SELECT key FROM #{DB_TABLE}").collect { |k| deserialize(k[0]) }
       end
     end
 
     def clear
-      EH::retry!(:args => []) do
+      EH.retry!(:args => []) do
         @storage_handler.execute("DELETE FROM #{DB_TABLE}")
       end
     end
@@ -67,7 +67,7 @@ module Persistent
     def deserialize(data)
       Marshal.load(Base64.decode64(data))
 
-      rescue TypeError => ex
+      rescue TypeError
         Marshal.load(data)
     end
 
@@ -76,13 +76,13 @@ module Persistent
     end
 
     def open_database
-      EH::retry!(:args => []) do
+      EH.retry!(:args => []) do
         SQLite3::Database.open(@storage_details)
       end
     end
 
     def create_database
-      EH::retry!(:args => []) do
+      EH.retry!(:args => []) do
         @storage_handler = SQLite3::Database.new(@storage_details)
         create_table
       end
@@ -90,7 +90,7 @@ module Persistent
     end
 
     def create_table
-      EH::retry!(:args => []) do
+      EH.retry!(:args => []) do
         @storage_handler.execute("CREATE TABLE #{DB_TABLE}(key TEXT PRIMARY KEY, value TEXT, timestamp TEXT)")
       end
       @storage_handler
